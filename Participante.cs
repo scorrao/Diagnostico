4c0745fd23033244ddd5f2217ba6c634a02da213
scorrao <scorrao@eest1chivilcoy.edu.ar>
2026-04-01 20:26:54 -0300

    Add initial project files and implement participant registration functionality

---
+++ Participante.cs
@@ -0,0 +1,8 @@
+public class  Participante {
+    public string Nombre {get;set;}
+    public string Apellido {get;set;}
+    public int Nro {get;set;}
+    public int Altura {get;set;}
+    public float Tiempo {get;set;}
+    public string Categoria {get;set;}
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("La carrera");
+Console.WriteLine();
+
+List<Participante> participantes = new List<Participante>();
+
+Participante p;
+do 
+{
+    p = new Participante();
+    Console.WriteLine("Ingrese el numero de participante:");
+    p.Nro = int.Parse(Console.ReadLine());
+    if (p.Nro == 0)
+        break;
+    Console.WriteLine("Ingrese el Nombre de participante:");
+    p.Nombre = Console.ReadLine();
+    Console.WriteLine("Ingrese el Apellido de participante:");
+    p.Apellido = Console.ReadLine();
+    Console.WriteLine("Ingrese la altura de participante:");
+    p.Altura = int.Parse(Console.ReadLine());
+    Console.WriteLine("Ingrese el Tiempo de participante:");
+    p.Tiempo = float.Parse(Console.ReadLine());
+    Console.WriteLine("Ingrese la Categoria de participante:");
+    p.Categoria = Console.ReadLine();
+    participantes.Add(p);
+    
+} while(true);
+
+
+Console.WriteLine($"La lista tiene {participantes.Count} participantes");
+
